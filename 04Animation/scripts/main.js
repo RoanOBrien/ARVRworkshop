bd7c2dd7aaa74bb0ba0aa67f809924decb893377
Roan O'Brien <17088445@student.hhs.nl>
2020-05-15 12:08:08 +0200

    Update main.js

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -10,34 +10,33 @@
  var depthInc = 0.2;
  var myOtherBox = document.getElementById('myOtherBox');
 
-// function spin(){
-// 	myOtherBox.object3D.rotation.x -= rotationSpeedX;
-//    myOtherBox.object3D.rotation.y += rotationSpeedY;
-//    myOtherBox.object3D.rotation.z += rotationSpeedZ;
-//    //myOtherBox.object3D.position.x -= rotationSpeedX;
-// 	console.log(myOtherBox.object3D.rotation.x);
-// }
+ function spin(){
+ 	myOtherBox.object3D.rotation.x -= rotationSpeedX;
+    myOtherBox.object3D.rotation.y += rotationSpeedY;
+    myOtherBox.object3D.rotation.z += rotationSpeedZ;
+    //myOtherBox.object3D.position.x -= rotationSpeedX;
+ 	console.log(myOtherBox.object3D.rotation.x);
+ }
 
 function grow(){
     //var decrease = 10;
-    myOtherBox.object3D.width += widthInc;
-//    for (var growthCheck = 0; growthCheck < 10; growthCheck++){
-//        myOtherBox.width += widthInc;
-//        myOtherBox.height += heightInc;
-//        myOtherBox.depth += depthInc;
-//        if (growthCheck == 9){
-//            decrease = 9;
-//        }
-//    }
-//    
-//    for (var decrease = 10; decrease > -1; decrease--){
-//        myOtherBox.width -= widthInc;
-//        myOtherBox.height -= heightInc;
-//        myOtherBox.depth -= depthInc;
-//        if (decrease == 0){
-//            growthCheck = 0;
-//        }
-//    }
+    for (var growthCheck = 0; growthCheck < 10; growthCheck++){
+        myOtherBox.object3D.scale.x += widthInc;
+        myOtherBox.object3D.scale.y += heightInc;
+        myOtherBox.object3D.scale.z += depthInc;
+        if (growthCheck == 9){
+            decrease = 9;
+        }
+    }
+    
+    for (var decrease = 10; decrease > -1; decrease--){
+        myOtherBox.object3D.scale.x -= widthInc;
+        myOtherBox.object3D.scale.y -= heightInc;
+        myOtherBox.object3D.scale.z -= depthInc;
+        if (decrease == 0){
+            growthCheck = 0;
+        }
+    }
     
     console.log(myOtherBox.object3D.width);
 }
