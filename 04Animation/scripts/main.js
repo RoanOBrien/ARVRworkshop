23e72d3011037d838e9b4a14e896046c589dc8b7
Roan O'Brien <17088445@student.hhs.nl>
2020-05-15 12:12:58 +0200

    Update main.js

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -5,18 +5,18 @@
  var rotationSpeedY = 0.01;
  var rotationSpeedZ = 0.04;
  //var growthCheck = 0;
- var widthInc = 0.2;
+ var widthInc = 0.02;
  var heightInc = 0.2;
  var depthInc = 0.2;
  var myOtherBox = document.getElementById('myOtherBox');
 
- function spin(){
- 	myOtherBox.object3D.rotation.x -= rotationSpeedX;
-    myOtherBox.object3D.rotation.y += rotationSpeedY;
-    myOtherBox.object3D.rotation.z += rotationSpeedZ;
-    //myOtherBox.object3D.position.x -= rotationSpeedX;
- 	console.log(myOtherBox.object3D.rotation.x);
- }
+// function spin(){
+// 	myOtherBox.object3D.rotation.x -= rotationSpeedX;
+//    myOtherBox.object3D.rotation.y += rotationSpeedY;
+//    myOtherBox.object3D.rotation.z += rotationSpeedZ;
+//    //myOtherBox.object3D.position.x -= rotationSpeedX;
+// 	console.log(myOtherBox.object3D.rotation.x);
+// }
 
 function grow(){
     //var decrease = 10;
